7dfa274a90263f50440e6a529059987f732c6ad8
IDOMATH <btdothemath@gmail.com>
2020-11-23 12:17:50 -0600

    QuoteExercise

---
+++ ExcerciseQuotes/ExcerciseQuotes/ExcerciseQuotes/QuotesPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExcerciseQuotes
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class QuotesPage : ContentPage
+    {
+        public static class Quotes
+        {
+            private static int index { get; set; }
+            private static readonly string[] QuoteString = new string[4] { "quote1", "quote2", "quote3", "quote4" };
+
+            static Quotes()
+            {
+                index = 0;
+            }
+
+            public static string GetQuote()
+            {
+                var quote = QuoteString[index];
+                index = (index + 1) % QuoteString.Length;
+                return quote;
+            }
+        }
+        public QuotesPage()
+        {
+            InitializeComponent();
+            fontSizeSlider.Value = 16;
+            quoteLabel.Text = Quotes.GetQuote();
+        }
+        public void NextQuoteButtonHandler(object sender, EventArgs e)
+        {
+            quoteLabel.Text = Quotes.GetQuote();
+        }
+    }
+}
