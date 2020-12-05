13840c6bf840c51bda22566f2e762c19381867f9
Blane <btdothemath@gmail.com>
2020-12-05 17:32:22 -0600

    Image Exercise has tons of outdated info
    Will return to this when needed.  Image lecture is vastly outdated.

---
+++ ExercisesImages/ExercisesImages/ExercisesImages/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ExercisesImages
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var imageSource = new UriImageSource { Uri = new Uri("http://lorempixel.com/1920/1080/city/1/") };
+            imageSource.CachingEnabled = false;
+        }
+    }
+}
